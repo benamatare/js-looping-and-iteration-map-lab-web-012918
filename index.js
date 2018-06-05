f2f8898403fe5486966d46c1eb1b64926e5ff6c2
github username <github email address>
2018-06-05 01:14:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file.
+
+lowerCaseDrivers = a => {
+  return a.map(el =>
+    el.toLowerCase()
+  )
+  // Map, then lowercase each element
+}
+
+nameToAttributes = a => {
+  return a.map(el =>
+    Object.assign({
+     firstName: el.split(' ')[0],
+      lastName: el.split(' ')[1]
+    })
+    // Map, then assign each split element into an object
+  )
+}
+
+attributesToPhrase = a => {
+  return a.map(el =>
+    `${el.name} is from ${el.hometown}`
+  )
+  // Map, then string interpolate!
+}
